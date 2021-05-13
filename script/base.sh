e3cbf426aed6e67d1166696225c1351c08b1cdf2
joyce04 <glee8804@gmail.com>
2021-05-13 01:32:40 -0700

    run both edge_split true/false

--- script/base.sh
+++ script/base.sh
@@ -4,44 +4,34 @@ tmux send-keys "conda activate ocean_g" C-m
 
 code=train_base.py
 dataset=cora
-config=./configs/gcn_base.json
-tmux send-keys "python $code --config $config & wait" C-m
+config=./configs/base.json
+tmux send-keys "python $code --config $config --train_ratio 0.01 & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.03 & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.05 & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.1 & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.15 & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.2 & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.25 & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.3 & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.35 & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.4 & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.45 & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.5 & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.55 & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.6 & wait" C-m
 
-train_ratio=0.03
-tmux send-keys "python $code --config $config --train_ratio $train_ratio & wait" C-m
-
-train_ratio=0.05
-tmux send-keys "python $code --config $config --train_ratio $train_ratio & wait" C-m
-
-train_ratio=0.1
-tmux send-keys "python $code --config $config --train_ratio $train_ratio & wait" C-m
-
-train_ratio=0.15
-tmux send-keys "python $code --config $config --train_ratio $train_ratio & wait" C-m
-
-train_ratio=0.2
-tmux send-keys "python $code --config $config --train_ratio $train_ratio & wait" C-m
-
-train_ratio=0.25
-tmux send-keys "python $code --config $config --train_ratio $train_ratio & wait" C-m
-
-train_ratio=0.3
-tmux send-keys "python $code --config $config --train_ratio $train_ratio & wait" C-m
-
-train_ratio=0.35
-tmux send-keys "python $code --config $config --train_ratio $train_ratio & wait" C-m
-
-train_ratio=0.4
-tmux send-keys "python $code --config $config --train_ratio $train_ratio & wait" C-m
-
-train_ratio=0.45
-tmux send-keys "python $code --config $config --train_ratio $train_ratio & wait" C-m
-
-train_ratio=0.5
-tmux send-keys "python $code --config $config --train_ratio $train_ratio & wait" C-m
-
-train_ratio=0.55
-tmux send-keys "python $code --config $config --train_ratio $train_ratio & wait" C-m
-
-train_ratio=0.6
-tmux send-keys "python $code --config $config --train_ratio $train_ratio & wait" C-m
+edge_split=false
+tmux send-keys "python $code --config $config --train_ratio 0.01 --edge_split $edge_split & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.03 --edge_split $edge_split & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.05 --edge_split $edge_split & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.1 --edge_split $edge_split & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.15 --edge_split $edge_split & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.2 --edge_split $edge_split & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.25 --edge_split $edge_split & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.3 --edge_split $edge_split & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.35 --edge_split $edge_split & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.4 --edge_split $edge_split & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.45 --edge_split $edge_split & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.5 --edge_split $edge_split & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.55 --edge_split $edge_split & wait" C-m
+tmux send-keys "python $code --config $config --train_ratio 0.6 --edge_split $edge_split & wait" C-m
