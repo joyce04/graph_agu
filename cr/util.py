9f5c5a854204ece55802169bc46ad956ae19bfe8
joyce04 <glee8804@gmail.com>
2021-05-20 06:12:48 -0700

    update

--- cr/util.py
+++ cr/util.py
@@ -2,4 +2,4 @@ from torch.nn import functional as F
 
 
 def compute_consistency(pred, label):
-    return F.kl_div(F.log_softmax(pred, dim=0), F.softmax(label, dim=0), reduction='mean')
+    return F.kl_div(F.log_softmax(pred, dim=0), F.softmax(label, dim=0), reduction='mean').cpu()
