fe2eec7e8ec34ee885f7177311d89d5097d1136e
joyce04 <glee8804@gmail.com>
2021-06-01 00:24:02 -0700

    update dropedge trial script

--- script/de_trial.sh
+++ script/de_trial.sh
@@ -1,13 +1,25 @@
 code=train_base.py
 config=./configs/de.json
 gnn_type=gcn
-python $code --config $config --data_split public
-python $code --config $config --data_split full
+python $code --config $config --data_split public --gnn $gnn_type --dataset "cora" --de_sampling_percent 0.7
+python $code --config $config --data_split full --gnn $gnn_type --dataset "cora" --de_sampling_percent 0.7
+python $code --config $config --data_split public --gnn $gnn_type --dataset "citeseer" --de_sampling_percent 0.05
+python $code --config $config --data_split full --gnn $gnn_type --dataset "citeseer" --de_sampling_percent 0.05
+python $code --config $config --data_split public --gnn $gnn_type --dataset "pubmed" --de_sampling_percent 0.3
+python $code --config $config --data_split full --gnn $gnn_type --dataset "pubmed" --de_sampling_percent 0.3
 
 gnn_type=graphsage
-python $code --config $config --data_split public
-python $code --config $config --data_split full
+python $code --config $config --data_split public --gnn $gnn_type --dataset "cora" --de_sampling_percent 0.4
+python $code --config $config --data_split full --gnn $gnn_type --dataset "cora" --de_sampling_percent 0.4
+python $code --config $config --data_split public --gnn $gnn_type --dataset "citeseer" --de_sampling_percent 0.1
+python $code --config $config --data_split full --gnn $gnn_type --dataset "citeseer" --de_sampling_percent 0.1
+python $code --config $config --data_split public --gnn $gnn_type --dataset "pubmed" --de_sampling_percent 0.8
+python $code --config $config --data_split full --gnn $gnn_type --dataset "pubmed" --de_sampling_percent 0.8
 
 gnn_type=gat
-python $code --config $config --data_split public
-python $code --config $config --data_split full
+python $code --config $config --data_split public --gnn $gnn_type --dataset "cora" --de_sampling_percent 0.7
+python $code --config $config --data_split full --gnn $gnn_type --dataset "cora" --de_sampling_percent 0.7
+python $code --config $config --data_split public --gnn $gnn_type --dataset "citeseer" --de_sampling_percent 0.05
+python $code --config $config --data_split full --gnn $gnn_type --dataset "citeseer" --de_sampling_percent 0.05
+python $code --config $config --data_split public --gnn $gnn_type --dataset "pubmed" --de_sampling_percent 0.8
+python $code --config $config --data_split full --gnn $gnn_type --dataset "pubmed" --de_sampling_percent 0.8
